1d600ad11f654100011a6467a492906fec66a486
Rap Navalez <rafaelnavalez@gmail.com>
2022-07-07 16:40:20 +0800

    initial page stylings and setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles.css';
+import Attribution from './components/Attribution'
+
+function App() {
+  return (
+    <div className="App">
+      <Attribution />
+    </div>
+  );
+}
+
+export default App;
